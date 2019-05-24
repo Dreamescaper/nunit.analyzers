fe25abcb5bf61146faf9b14228a381c8ed5423d5
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2019-05-25 02:16:33 +0300

    Refactor assertion analyzers

--- src/nunit.analyzers/ConstActualValueUsage/ConstActualValueUsageAnalyzer.cs
+++ src/nunit.analyzers/ConstActualValueUsage/ConstActualValueUsageAnalyzer.cs
@@ -1,16 +1,15 @@
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using NUnit.Analyzers.Constants;
-using NUnit.Analyzers.Extensions;
+using NUnit.Analyzers.Helpers;
 
 namespace NUnit.Analyzers.ConstActualValueUsage
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class ConstActualValueUsageAnalyzer : DiagnosticAnalyzer
+    public class ConstActualValueUsageAnalyzer : BaseAssertionAnalyzer
     {
         private static readonly DiagnosticDescriptor descriptor = new DiagnosticDescriptor(
             AnalyzerIdentifiers.ConstActualValueUsage,
@@ -22,43 +21,23 @@ namespace NUnit.Analyzers.ConstActualValueUsage
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(descriptor);
 
-        public override void Initialize(AnalysisContext context)
+        protected override void AnalyzeAssertInvocation(SyntaxNodeAnalysisContext context,
+            InvocationExpressionSyntax invocationSyntax, IMethodSymbol methodSymbol)
         {
-            context.EnableConcurrentExecution();
+            var actualExpression = AssertExpressionHelper.TryGetActualValue(invocationSyntax, methodSymbol);
 
-            context.RegisterSyntaxNodeAction(AnalyzeInvocation, SyntaxKind.Argument);
-        }
-
-        private static void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
-        {
-            var argumentSyntax = context.Node as ArgumentSyntax;
-            var argumentListSyntax = argumentSyntax?.Parent as ArgumentListSyntax;
-            var invocationSyntax = argumentSyntax?.Ancestors().OfType<InvocationExpressionSyntax>().FirstOrDefault();
-
-            if (argumentSyntax == null || argumentListSyntax == null || invocationSyntax == null)
+            if (actualExpression == null)
                 return;
 
-            var symbol = context.SemanticModel.GetSymbolInfo(invocationSyntax.Expression).Symbol;
+            var argumentSymbol = context.SemanticModel.GetSymbolInfo(actualExpression).Symbol;
 
-            if (!(symbol is IMethodSymbol methodSymbol) || !methodSymbol.ContainingType.IsAssert())
-                return;
-
-            var parameterSymbol = argumentSyntax.NameColon != null
-                ? methodSymbol.Parameters.FirstOrDefault(p => p.Name == argumentSyntax.NameColon.Name.Identifier.Text)
-                : methodSymbol.Parameters.ElementAtOrDefault(argumentListSyntax.Arguments.IndexOf(argumentSyntax));
-
-            if (parameterSymbol?.Name == NunitFrameworkConstants.NameOfActualParameter)
+            if (actualExpression is LiteralExpressionSyntax
+                || (argumentSymbol is ILocalSymbol localSymbol && localSymbol.IsConst)
+                || (argumentSymbol is IFieldSymbol fieldSymbol && fieldSymbol.IsConst))
             {
-                var argumentSymbol = context.SemanticModel.GetSymbolInfo(argumentSyntax.Expression).Symbol;
-
-                if (argumentSyntax.Expression is LiteralExpressionSyntax
-                    || (argumentSymbol is ILocalSymbol localSymbol && localSymbol.IsConst)
-                    || (argumentSymbol is IFieldSymbol fieldSymbol && fieldSymbol.IsConst))
-                {
-                    context.ReportDiagnostic(Diagnostic.Create(
-                        descriptor,
-                        argumentSyntax.GetLocation()));
-                }
+                context.ReportDiagnostic(Diagnostic.Create(
+                    descriptor,
+                    actualExpression.GetLocation()));
             }
         }
     }
